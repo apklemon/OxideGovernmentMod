c80e333538693751464773ba2dedecbbfbbaaa30
hsalem <hazim22291@hotmail.com>
2015-10-14 17:47:33 -0500

    Added class Nation

--- Nations.cs
+++ Nations.cs
@@ -16,6 +16,41 @@ namespace Oxide.Plugins
     [Info("Nations", "BodyweightEnergy", "0.0.1", ResourceId = 0)]
     public class Nations : RustPlugin
     {
-        
+        enum Rank
+        {
+            ENEMY = -2,
+            BANISHED = -1,
+            NONE = 0,
+            INVITEE = 1,
+            GUEST = 2,
+            CITIZEN = 3,
+            BUILDER = 4,
+            BOOT = 5,
+            HEAD = 6,
+            DICTATOR = 7
+        }
+
+        public class Nation
+        {
+            public string Tag { get; set; }
+            public string Name { get; set; }
+            public Dictionary<ulong, Rank> Members { get; }
+            public Dictionary<ulong, String> MemberNotes { get; }
+        }
+
+        public class GroupManager
+        {
+            
+        }
+
+        public class PermissionManager
+        {
+
+        }
+
+        public class UIManager
+        {
+
+        }
     }
 }
